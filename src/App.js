abf948d838fdfe906d049dceaebf2316754c1a3b
Ryan Park <ryan.developer99@gmail.com>
2021-06-06 15:54:47 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Setup from "./tutorial/4-forms/controlled-inputs";
+
+function App() {
+  return (
+    <div>
+      <Setup />
+    </div>
+  );
+}
+
+export default App;
